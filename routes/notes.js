79ff03b52a4a475f845f2052f49e81e159caf7fa
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-05-03 15:48:42 -0600

    added helpers, routes. Will push to main, then create a branch and reset back to starting code. Want to do more from scratch rather than pull from the mi ni project so excessively.

---
+++ routes/notes.js
@@ -0,0 +1,28 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
+
+module.exports = notes;
